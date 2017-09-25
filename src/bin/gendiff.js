899a54608146eb79692b2322622673afbb223817
Ivan Rastyapin <i.rastypain@gmail.com>
2017-09-25 17:37:31 +0300

    Add program with print help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+import program from 'commander';
+import config from '../../package.json';
+
+program
+  .version(config.version)
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv);
