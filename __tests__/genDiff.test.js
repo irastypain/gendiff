07f124282e50ab32d2b61f968986081150d94f75
Ivan Rastyapin <i.rastypain@gmail.com>
2017-09-26 12:50:11 +0300

    Add __fixtures__ folder. Remove lib folder

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,8 +1,10 @@
-import genDiff from '../src/lib/genDiff';
+import genDiff from '../src/index';
+
+const pathToFixtures = `${__dirname}/__fixtures__`;
 
 test('compare two flat .json files ', () => {
   const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
-  const pathToFstFile = `${__dirname}/json/before.json`;
-  const pathToSndFile = `${__dirname}/json/after.json`;
+  const pathToFstFile = `${pathToFixtures}/json/before.json`;
+  const pathToSndFile = `${pathToFixtures}/json/after.json`;
   expect(genDiff(pathToFstFile, pathToSndFile)).toBe(expected);
 });
