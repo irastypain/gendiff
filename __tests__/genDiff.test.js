f41d5683e7946204fc31d52d7712d084bbedff50
Ivan Rastyapin <i.rastypain@gmail.com>
2017-09-26 16:47:21 +0300

    Add test for compare .yaml files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,10 +1,16 @@
 import genDiff from '../src/index';
 
 const pathToFixtures = `${__dirname}/__fixtures__`;
+const expectedFlat = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
 
-test('compare two flat .json files ', () => {
-  const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
+test('compare two flat .json files', () => {
   const pathToFstFile = `${pathToFixtures}/json/before.json`;
   const pathToSndFile = `${pathToFixtures}/json/after.json`;
-  expect(genDiff(pathToFstFile, pathToSndFile)).toBe(expected);
+  expect(genDiff(pathToFstFile, pathToSndFile)).toBe(expectedFlat);
+});
+
+test('compare two flat .yaml files', () => {
+  const pathToFstFile = `${pathToFixtures}/yaml/before.yaml`;
+  const pathToSndFile = `${pathToFixtures}/yaml/after.yaml`;
+  expect(genDiff(pathToFstFile, pathToSndFile)).toBe(expectedFlat);
 });
