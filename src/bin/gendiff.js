d1efa035cdc182926d2f649a789e070473e80b7d
Ivan Rastyapin <i.rastypain@gmail.com>
2017-12-23 14:06:51 +0300

    Fix help text

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,7 +7,7 @@ import genDiff from '../index';
 program
   .version(config.version)
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format')
+  .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
     console.log(genDiff(firstConfig, secondConfig, program.format));
